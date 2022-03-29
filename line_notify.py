fce2131e6722600250bde7e2134703efda07e1de
rhy3h <rhy3htw@gmail.com>
2022-03-29 19:34:22 +0800

    more status

--- line_notify.py
+++ line_notify.py
@@ -20,21 +20,24 @@ if __name__ == "__main__":
     
     while True:
         channelStatus = isChannelLive(client_id, client_secret, streamer_name)
-        if len(channelStatus) == 1 and live_status == False:
-            live_status = True
-            message = channelStatus[0]['user_name'] + '開台囉\n' + channelStatus[0]['title'] + '\nhttps://twitch.tv/' + channelStatus[0]['user_login']
-            params = {
-                "message": message
-            }
-            r = requests.post(
-                "https://notify-api.line.me/api/notify",
-                headers = headers,
-                params = params
-            )
-            print(channelStatus[0]['user_name'] + "開台囉")
-        elif live_status == True and len(channelStatus) == 0:
-            live_status = False
-            print(channelStatus[0]['user_name']  + "關台了")
-        else:
-            print("離線中")
+        if live_status == False:
+            if len(channelStatus) == 1:
+                live_status = True
+                message = channelStatus[0]['user_name'] + '開台囉\n' + channelStatus[0]['title'] + '\nhttps://twitch.tv/' + channelStatus[0]['user_login']
+                params = {
+                    "message": message
+                }
+                r = requests.post(
+                    "https://notify-api.line.me/api/notify",
+                    headers = headers,
+                    params = params
+                )
+                print(channelStatus[0]['user_name'] + "開台囉")
+            else:
+                print("離線中")
+        if live_status == True:
+            if len(channelStatus) == 1:
+                print(channelStatus[0]['user_name'] + "持續開台中")
+            else:
+                print("離線中")
         sleep(5)
